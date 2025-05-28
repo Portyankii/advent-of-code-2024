eff0a109817ecb5eb371cba6b6b09fb083fcb259
gabe <gabe@notsle.com>
2025-05-28 14:10:37 -0400

    day 2 works now :]

--- day3/day3p2.lisp
+++ day3/day3p2.lisp
@@ -1,15 +1,37 @@
 (ql:quickload :split-sequence)
 (ql:quickload :cl-ppcre)
 
+(defparameter *do* t)
 
-(defun mul (x y)
-  "im to lazy to convert mul to *. that is the only reason this exists"
-  (* x y))
+(defun MUL (x y)
+  "if do is true, return x*y, if do is nil, return 0"
+  (if *do*
+      (* x y)
+      0))
 
-(defun filter-do)
+(defun SET-DO ()
+  (setf *do* t) 0)
+
+(defun SET-DONT ()
+  (setf *do* nil) 0)
+
+
 (defun filter-slop (str)
-  (cl-ppcre:all-matches-as-strings "mul\\([0-9]+,[0-9]+\\)" str))
+  (cl-ppcre:all-matches-as-strings "mul\\([0-9]+,[0-9]+\\)|don't\\(\\)|do\\(\\)" str))
 
+(defun fix-do (lst)
+  (mapcar (lambda (s)
+            (if (string= s "do()")
+                "set-do()"
+                s))
+          lst))
+
+(defun fix-dont (lst)
+  (mapcar (lambda (s)
+            (if (string= s "don't()")
+                "set-dont()"
+                s))
+          lst))
 
 (defun parse-c-call (s)
   "Parse a C-style call string like \"foo(1,2)\" to a Lisp list like (foo 1 2)."
@@ -17,9 +39,12 @@
          (close-paren (position #\) s :from-end t))
          (func-name (subseq s 0 open-paren))
          (args-str (subseq s (1+ open-paren) close-paren))
-         (args (mapcar #'read-from-string
-                       (split-sequence:split-sequence #\, args-str))))
-    `(,(intern (string-upcase func-name) :cl-user) ,@args)))
+         (args (if (string= args-str "")
+                   '()
+                   (mapcar #'read-from-string
+                           (split-sequence:split-sequence #\, args-str)))))
+    (list* (intern (string-upcase func-name) :cl-user) args)))
+
 
 (defun parse-list-to-forms (lst)
   "Convert a list of C-style strings to a list of Lisp forms (do not evaluate)."
@@ -37,7 +62,20 @@
                                    :if-does-not-exist :create)
     (pprint lst stream)))
 
-(write-list-as-sexp
- (parse-list-to-forms (filter-slop (read-file-string "/home/rye/development/lisp/advent-of-code-2024/day2/day3/day3.dat")))
- "output.txt")
-(parse-list-to-forms (filter-slop (read-file-string "/home/rye/development/lisp/advent-of-code-2024/day2/day3/day3.dat")))
+(defun execute-instructions-sequentially (forms)
+  "Execute a list of forms sequentially, summing the numeric results."
+  (let ((sum 0))
+    (dolist (form forms sum)
+      (let ((result (eval form)))
+        (when (numberp result)
+          (incf sum result))))))
+(setf *do* t)
+
+(let ((result (execute-instructions-sequentially
+               (parse-list-to-forms
+                (fix-dont
+                 (fix-do
+                  (filter-slop
+                   (read-file-string "/home/rye/development/lisp/advent-of-code-2024/day3/day3.dat"))))))))
+  (format t "Result: ~a~%" result)
+  result)
